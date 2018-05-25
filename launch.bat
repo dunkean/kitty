22a3cb1b6e9173ef8b239396b3024034b1e0b89d
dunkean <dunkean@gmail.com>
2018-05-25 16:27:16 +0200

    tuned watch launch in windows

--- launch.bat
+++ launch.bat
@@ -1,6 +1,5 @@
 start npm run win-start-api
-REM start npm run theme:watch
-REM start npm run webpack:store:watch
-REM start pm2 start store.json
+start npm run win-theme:watch
+start npm run win-webpack:store:watch
+mode con:cols=40 lines=30 && pm2 start store.json
 REM start ..../mongod.exe -dbpath d:\mongodb
-REM "win-start-api": "mode con:cols=30 lines=20 && node src/api/server/index.js"
