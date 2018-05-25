1ffa7708ff76b07bf42b73f1e511dec5cb14f0ba
dunkean <dunkean@gmail.com>
2018-05-25 16:14:27 +0200

    modified launch script

--- launch.bat
+++ launch.bat
@@ -1,6 +1,6 @@
-npm run start-api
-npm run theme:watch
-npm run webpack:store:watch
-pm2 start store.json
-
-#..../mongod.exe -dbpath d:\mongodb
+start npm run win-start-api
+REM start npm run theme:watch
+REM start npm run webpack:store:watch
+REM start pm2 start store.json
+REM start ..../mongod.exe -dbpath d:\mongodb
+REM "win-start-api": "mode con:cols=30 lines=20 && node src/api/server/index.js"
